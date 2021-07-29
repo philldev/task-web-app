1d32279894383e2df3a3471aab741850ae70dcca
Deddy Wolley <deddywolley@gmail.com>
2021-07-29 23:21:59 +0800

    added base boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+	return (
+		<div className='min-h-screen bg-gray-900 text-gray-50 w-full'>todo app</div>
+	)
 }
-
-export default App;
